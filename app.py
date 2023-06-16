524064137499110a1ff78516b9b75fc5141ec82a
sanket <telisanket2002@gmail.com>
2023-06-16 09:17:01 +0530

    finished the home page

--- app.py
+++ app.py
@@ -2,8 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+    'title': 'frontend',
+    'location': 'mumbai',
+    'salary': 100000
+},{
+     'title': 'backend',
+    'location': 'bangloru',
+    'salary': 200000
+},{
+     'title': 'full stack',
+    'location': 'kalyan',
+    'salary': 300000
+}]
 @app.route("/")
 def run():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
 
 
